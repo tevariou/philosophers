9e43fde806d86d5a5f91239b5247ee1376e9976f
Teva RIOU <triou@vente-privee.com>
2020-07-25 17:52:56 +0200

    norme

--- philo_one/philo_one.c
+++ philo_one/philo_one.c
@@ -1,7 +1,7 @@
 #include "philo_one.h"
 #include <unistd.h>
 
-int config(t_config *conf, int ac, char **av)
+int     config(t_config *conf, int ac, char **av)
 {
 	if (ac < 5) {
 	    write(2, USAGE, ft_strlen(USAGE));
@@ -20,7 +20,11 @@ int config(t_config *conf, int ac, char **av)
 	return (EXIT_SUCCESS);
 }
 
-void init(t_philosopher *philosopher_array, pthread_mutex_t *fork_array, t_config *main_conf)
+void    init(
+            t_philosopher *philosopher_array,
+            pthread_mutex_t *fork_array,
+            t_config *main_conf
+        )
 {
     size_t          i;
     size_t          number_of_philosopher;
@@ -41,35 +45,70 @@ void init(t_philosopher *philosopher_array, pthread_mutex_t *fork_array, t_confi
     }
 }
 
-int main(int ac, char** av) {
+void    clean(
+        t_philosopher *philosopher_array,
+        pthread_mutex_t *fork_array,
+        t_config *conf
+) {
+    size_t i;
+
+    i = 0;
+    while (i < conf->number_of_philosopher) {
+        pthread_mutex_destroy(&fork_array[i]);
+        i++;
+    }
+    pthread_mutex_destroy(&conf->mutex);
+    free(philosopher_array);
+    free(fork_array);
+}
+
+int     run(
+            t_philosopher *philosopher_array,
+            pthread_mutex_t *fork_array,
+            t_config *conf
+        ) {
+    size_t i;
+    pthread_t monitor;
+
+    i = 0;
+    while (i < conf->number_of_philosopher) {
+        if (pthread_create(&philosopher_array[i].thread, NULL, &philosopher_run, &philosopher_array[i])) {
+            clean(philosopher_array, fork_array, conf);
+            return (EXIT_FAILURE);
+        }
+        i++;
+    }
+    i = 0;
+    while (i < conf->number_of_philosopher) {
+        pthread_detach(philosopher_array[i].thread);
+        i++;
+    }
+    if (pthread_create(&monitor, NULL, &monitor_run, philosopher_array)) {
+        clean(philosopher_array, fork_array, conf);
+        return (EXIT_FAILURE);
+    }
+    pthread_join(monitor, NULL);
+    return (EXIT_SUCCESS);
+}
+
+int     main(int ac, char** av) {
 	t_config        conf;
 	pthread_mutex_t *fork_array;
 	t_philosopher   *philosopher_array;
-	pthread_t       monitor;
-	size_t          i;
+    size_t          size;
 
 	if (config(&conf, ac, av)) {
 		return (EXIT_FAILURE);
 	}
-	if (!(fork_array = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * conf.number_of_philosopher))) {
+	size = sizeof(pthread_mutex_t) * conf.number_of_philosopher;
+	if (!(fork_array = (pthread_mutex_t *)malloc(size))) {
         return (EXIT_FAILURE);
     }
-    if (!(philosopher_array = (t_philosopher *)malloc(sizeof(*philosopher_array) * conf.number_of_philosopher))) {
+	size = sizeof(*philosopher_array) * conf.number_of_philosopher;
+    if (!(philosopher_array = (t_philosopher *)malloc(size))) {
         free(fork_array);
         return (EXIT_FAILURE);
     }
 	init(philosopher_array, fork_array, &conf);
-	i = 0;
-	while (i < conf.number_of_philosopher) {
-	    pthread_create(&philosopher_array[i].thread, NULL, &philosopher_run, &philosopher_array[i]);
-	    i++;
-	}
-	i = 0;
-    while (i < conf.number_of_philosopher) {
-        pthread_detach(philosopher_array[i].thread);
-        i++;
-    }
-    pthread_create(&monitor, NULL, &monitor_run, philosopher_array);
-    pthread_join(monitor, NULL);
-	return (EXIT_SUCCESS);
+	return (run(philosopher_array, fork_array, &conf));
 }
