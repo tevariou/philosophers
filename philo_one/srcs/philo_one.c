d5585dbdcc8d9423f400a7a33b2e35b2ca364c8e
Teva RIOU <teva@yusofleet.com>
2020-08-01 19:20:18 +0200

    fix: indents

--- philo_one/srcs/philo_one.c
+++ philo_one/srcs/philo_one.c
@@ -1,103 +1,103 @@
 #include "philo_one.h"
 
 static int  init(
-            t_philosopher   *philosopher_array,
-            pthread_mutex_t *fork_array,
-            t_config        *main_conf
-        )
+			t_philosopher   *philosopher_array,
+			pthread_mutex_t *fork_array,
+			t_config        *main_conf
+		)
 {
-    size_t          i;
-    size_t          n;
+	size_t          i;
+	size_t          n;
 
-    n = main_conf->number_of_philosopher;
-    i = 0;
-    while (i < n)
-    {
-        if (pthread_mutex_init(&fork_array[i], NULL))
-        {
-            clean(philosopher_array, fork_array, &main_conf->mutex, i + 1);
-            return (EXIT_FAILURE);
-        }
-        philosopher_array[i].conf = main_conf;
-        philosopher_array[i].left = fork_array + i;
-        philosopher_array[i].right = i
-                ? fork_array + i - 1 : fork_array + n - 1;
-        philosopher_array[i].number = i;
-        philosopher_array[i].state.counter = 0;
-        philosopher_array[i].state.last_eating.tv_sec = 0;
-        philosopher_array[i].state.last_eating.tv_usec = 0;
-        i++;
-    }
-    return (EXIT_SUCCESS);
+	n = main_conf->number_of_philosopher;
+	i = 0;
+	while (i < n)
+	{
+		if (pthread_mutex_init(&fork_array[i], NULL))
+		{
+			clean(philosopher_array, fork_array, &main_conf->mutex, i + 1);
+			return (EXIT_FAILURE);
+		}
+		philosopher_array[i].conf = main_conf;
+		philosopher_array[i].left = fork_array + i;
+		philosopher_array[i].right = i
+				? fork_array + i - 1 : fork_array + n - 1;
+		philosopher_array[i].number = i;
+		philosopher_array[i].state.counter = 0;
+		philosopher_array[i].state.last_eating.tv_sec = 0;
+		philosopher_array[i].state.last_eating.tv_usec = 0;
+		i++;
+	}
+	return (EXIT_SUCCESS);
 }
 
 static void  pwait(t_philosopher *philo_array, t_config *conf, pthread_t *monitor) {
-    size_t  i;
+	size_t  i;
 
-    i = 0;
-    while (i < conf->number_of_philosopher)
-    {
-        pthread_join(philo_array[i].thread, NULL);
-        i++;
-    }
-    pthread_join(*monitor, NULL);
+	i = 0;
+	while (i < conf->number_of_philosopher)
+	{
+		pthread_join(philo_array[i].thread, NULL);
+		i++;
+	}
+	pthread_join(*monitor, NULL);
 }
 
 static int  run(
-            t_philosopher   *philo_array,
-            pthread_mutex_t *fork_array,
-            t_config        *conf
-        )
+			t_philosopher   *philo_array,
+			pthread_mutex_t *fork_array,
+			t_config        *conf
+		)
 {
-    size_t      i;
-    size_t      n;
-    pthread_t   monitor;
-    void        *(*f)(void *);
+	size_t      i;
+	size_t      n;
+	pthread_t   monitor;
+	void        *(*f)(void *);
 
-    n = conf->number_of_philosopher;
-    i = 0;
-    while (i < n)
-    {
-        f = (i % 2 == 0) ? &even_philosopher_run : &odd_philosopher_run;
-        if (pthread_create(&philo_array[i].thread, NULL, f, philo_array + i))
-        {
-            clean(philo_array, fork_array, &conf->mutex, n);
-            return (EXIT_FAILURE);
-        }
-        i++;
-    }
-    if (pthread_create(&monitor, NULL, &monitor_run, philo_array))
-    {
-        clean(philo_array, fork_array, &conf->mutex, n);
-        return (EXIT_FAILURE);
-    }
-    pwait(philo_array, conf, &monitor);
-    clean(philo_array, fork_array, &conf->mutex, n);
-    return (EXIT_SUCCESS);
+	n = conf->number_of_philosopher;
+	i = 0;
+	while (i < n)
+	{
+		f = (i % 2 == 0) ? &even_philosopher_run : &odd_philosopher_run;
+		if (pthread_create(&philo_array[i].thread, NULL, f, philo_array + i))
+		{
+			clean(philo_array, fork_array, &conf->mutex, n);
+			return (EXIT_FAILURE);
+		}
+		i++;
+	}
+	if (pthread_create(&monitor, NULL, &monitor_run, philo_array))
+	{
+		clean(philo_array, fork_array, &conf->mutex, n);
+		return (EXIT_FAILURE);
+	}
+	pwait(philo_array, conf, &monitor);
+	clean(philo_array, fork_array, &conf->mutex, n);
+	return (EXIT_SUCCESS);
 }
 
 static int  alloc(
-            pthread_mutex_t **fork_array,
-            t_philosopher   **philosopher_array,
-            t_config        *conf
-        )
+			pthread_mutex_t **fork_array,
+			t_philosopher   **philosopher_array,
+			t_config        *conf
+		)
 {
-    size_t size;
+	size_t size;
 
-    size = sizeof(pthread_mutex_t) * conf->number_of_philosopher;
-    if (!(*fork_array = (pthread_mutex_t *)malloc(size)))
-    {
-        pthread_mutex_destroy(&conf->mutex);
-        return (EXIT_FAILURE);
-    }
-    size = sizeof(t_philosopher) * conf->number_of_philosopher;
-    if (!(*philosopher_array = (t_philosopher *)malloc(size)))
-    {
-        pthread_mutex_destroy(&conf->mutex);
-        free(*fork_array);
-        return (EXIT_FAILURE);
-    }
-    return (EXIT_SUCCESS);
+	size = sizeof(pthread_mutex_t) * conf->number_of_philosopher;
+	if (!(*fork_array = (pthread_mutex_t *)malloc(size)))
+	{
+		pthread_mutex_destroy(&conf->mutex);
+		return (EXIT_FAILURE);
+	}
+	size = sizeof(t_philosopher) * conf->number_of_philosopher;
+	if (!(*philosopher_array = (t_philosopher *)malloc(size)))
+	{
+		pthread_mutex_destroy(&conf->mutex);
+		free(*fork_array);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
 
 int         main(int ac, char** av)
@@ -106,11 +106,11 @@ int         main(int ac, char** av)
 	pthread_mutex_t *fork_array;
 	t_philosopher   *philosopher_array;
 
-    if (config(&conf, ac, av))
-        return (EXIT_FAILURE);
-    if (alloc(&fork_array, &philosopher_array, &conf))
-        return (EXIT_FAILURE);
+	if (config(&conf, ac, av))
+		return (EXIT_FAILURE);
+	if (alloc(&fork_array, &philosopher_array, &conf))
+		return (EXIT_FAILURE);
 	if (init(philosopher_array, fork_array, &conf))
-	    return (EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	return (run(philosopher_array, fork_array, &conf));
 }
--- philo_one/srcs/print.c
+++ philo_one/srcs/print.c
@@ -3,25 +3,25 @@
 #include <string.h>
 
 int print_status(const char *status, size_t number, t_config *conf) {
-    struct timeval time;
-    char           str[34];
+	struct timeval time;
+	char           str[34];
 
-    gettimeofday(&time, NULL);
-    pthread_mutex_lock(&conf->mutex);
-    if (conf->is_finished) {
-        pthread_mutex_unlock(&conf->mutex);
-        return (EXIT_FAILURE);
-    }
-    if (!ft_strcmp("died", status))
-        conf->is_finished = true;
-    memset(str, 0, 34);
-    ft_putnbr(str, timeval_to_msec(time));
-    ft_append(str, " ");
-    ft_putnbr(str, number);
-    ft_append(str, " ");
-    ft_append(str, status);
-    ft_append(str, "\n");
-    ft_putstr_fd(str, 1);
-    pthread_mutex_unlock(&conf->mutex);
-    return (EXIT_SUCCESS);
+	gettimeofday(&time, NULL);
+	pthread_mutex_lock(&conf->mutex);
+	if (conf->is_finished) {
+		pthread_mutex_unlock(&conf->mutex);
+		return (EXIT_FAILURE);
+	}
+	if (!ft_strcmp("died", status))
+		conf->is_finished = true;
+	memset(str, 0, 34);
+	ft_putnbr(str, timeval_to_msec(time));
+	ft_append(str, " ");
+	ft_putnbr(str, number);
+	ft_append(str, " ");
+	ft_append(str, status);
+	ft_append(str, "\n");
+	ft_putstr_fd(str, 1);
+	pthread_mutex_unlock(&conf->mutex);
+	return (EXIT_SUCCESS);
 }
