bf1626eb7eb4eaf4a1a16e00ff6211f49ca235f8
Teva RIOU <teva@yusofleet.com>
2020-08-01 20:46:47 +0200

    norme

--- philo_one/includes/philo_one.h
+++ philo_one/includes/philo_one.h
@@ -1,57 +1,57 @@
-#ifndef             PHILOSOPHERS_PHILO_ONE_H
-#define             PHILOSOPHERS_PHILO_ONE_H
+#ifndef				PHILOSOPHERS_PHILO_ONE_H
+#define				PHILOSOPHERS_PHILO_ONE_H
 
-#include            <pthread.h>
-#include            <sys/time.h>
-#include            <stdlib.h>
-#include            <stdbool.h>
+#include			<pthread.h>
+#include			<sys/time.h>
+#include			<stdlib.h>
+#include			<stdbool.h>
 #include			<stdint.h>
 
-#define             USAGE        "Usage: ./philosopher number_of_philosopher " \
-								 "time_to_die time_to_eat time_to_sleep " \
-								 "[number_of_time_each_philosophers_must_eat]\n"
-#define             PARAMS       "Params must be short numbers\n"
+#define				USAGE	"Usage: ./philosopher number_of_philosopher " \
+							"time_to_die time_to_eat time_to_sleep " \
+							"[number_of_time_each_philosophers_must_eat]\n"
+#define             PARAMS	"Params must be short numbers\n"
 
-typedef struct      s_state {
-	struct timeval  last_eating;
-	int             counter;
-}                   t_state;
+typedef struct		s_state {
+	struct timeval	last_eating;
+	int				counter;
+}					t_state;
 
-typedef struct      s_config {
-	size_t          number_of_philosopher;
-	int             time_to_die;
-	int             time_to_eat;
-	int             time_to_sleep;
-	int             number_of_time_each_philosophers_must_eat;
-	pthread_mutex_t mutex;
-	bool            is_finished;
-}                   t_config;
+typedef struct		s_config {
+	size_t			number_of_philosopher;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_time_each_philosophers_must_eat;
+	pthread_mutex_t	mutex;
+	bool			is_finished;
+}					t_config;
 
-typedef struct      s_philosopher {
-	size_t          number;
-	pthread_t       thread;
-	t_state         state;
-	pthread_mutex_t *left;
-	pthread_mutex_t *right;
-	t_config        *conf;
-}                   t_philosopher;
+typedef struct		s_philosopher {
+	size_t			number;
+	pthread_t		thread;
+	t_state			state;
+	pthread_mutex_t	*left;
+	pthread_mutex_t	*right;
+	t_config		*conf;
+}					t_philosopher;
 
-size_t	            ft_strlen(const char *s);
-short	            ft_atos(const char *str);
-void	            ft_putnbr(char *s, uint64_t n);
-void                ft_append(char *dest, const char *str);
-void	            ft_putstr_fd(char const *s, int fd);
-int	                ft_strcmp(const char *s1, const char *s2);
-uint64_t            timeval_to_msec(struct timeval time);
-int                 timeval_cmp(struct timeval a, struct timeval b);
-struct timeval      timeval_add(struct timeval a, unsigned int b);
-int                 print_status(const char *status, size_t number,
+size_t				ft_strlen(const char *s);
+short				ft_atos(const char *str);
+void				ft_putnbr(char *s, uint64_t n);
+void				ft_append(char *dest, const char *str);
+void				ft_putstr_fd(char const *s, int fd);
+int					ft_strcmp(const char *s1, const char *s2);
+uint64_t			timeval_to_msec(struct timeval time);
+int					timeval_cmp(struct timeval a, struct timeval b);
+struct timeval		timeval_add(struct timeval a, unsigned int b);
+int					print_status(const char *status, size_t number,
 						t_config *conf);
-void                *even_philosopher_run(void *arg);
-void                *odd_philosopher_run(void *arg);
-void                *monitor_run(void *arg);
-int                 config(t_config *conf, int ac, char **av);
-void                clean(t_philosopher *philosopher_array,
+void				*even_philosopher_run(void *arg);
+void				*odd_philosopher_run(void *arg);
+void				*monitor_run(void *arg);
+int					config(t_config *conf, int ac, char **av);
+void				clean(t_philosopher *philosopher_array,
 						pthread_mutex_t *fork_array,pthread_mutex_t
 						*mutex,size_t n);
 
--- philo_three/includes/philo_three.h
+++ philo_three/includes/philo_three.h
@@ -1,53 +1,53 @@
-#ifndef             PHILOSOPHERS_PHILO_THREE_H
-#define             PHILOSOPHERS_PHILO_THREE_H
+#ifndef				PHILOSOPHERS_PHILO_THREE_H
+#define				PHILOSOPHERS_PHILO_THREE_H
 
-#include            <pthread.h>
-#include            <sys/time.h>
-#include            <stdlib.h>
-#include            <stdbool.h>
-#include            <semaphore.h>
+#include			<pthread.h>
+#include			<sys/time.h>
+#include			<stdlib.h>
+#include			<stdbool.h>
+#include			<semaphore.h>
 #include			<stdint.h>
 
-#define             USAGE        "Usage: ./philosopher number_of_philosopher " \
-								 "time_to_die time_to_eat time_to_sleep " \
-								 "[number_of_time_each_philosophers_must_eat]\n"
-#define             PARAMS       "Params must be short numbers\n"
+#define				USAGE	"Usage: ./philosopher number_of_philosopher " \
+							"time_to_die time_to_eat time_to_sleep " \
+							"[number_of_time_each_philosophers_must_eat]\n"
+#define				PARAMS	"Params must be short numbers\n"
 
-typedef struct      s_state {
-	struct timeval  last_eating;
-	int             counter;
-}                   t_state;
+typedef struct		s_state {
+	struct timeval	last_eating;
+	int				counter;
+}					t_state;
 
-typedef struct      s_config {
-	size_t          number_of_philosopher;
-	int    			time_to_die;
-	int    			time_to_eat;
+typedef struct		s_config {
+	size_t			number_of_philosopher;
+	int				time_to_die;
+	int				time_to_eat;
 	int				time_to_sleep;
-	int             number_of_time_each_philosophers_must_eat;
-	sem_t           *forks;
-	sem_t           *print;
-}                   t_config;
+	int				number_of_time_each_philosophers_must_eat;
+	sem_t			*forks;
+	sem_t			*print;
+}					t_config;
 
-typedef struct      s_philosopher {
-	size_t          number;
-	pthread_t       thread;
-	t_state         state;
-	t_config        *conf;
-}                   t_philosopher;
+typedef struct		s_philosopher {
+	size_t			number;
+	pthread_t		thread;
+	t_state			state;
+	t_config		*conf;
+}					t_philosopher;
 
-size_t	            ft_strlen(const char *s);
-short	            ft_atos(const char *str);
-void	            ft_putnbr(char *s, uint64_t n);
-void	            ft_append(char *dest, const char *str);
-void	            ft_putstr_fd(char const *s, int fd);
-int	                ft_strcmp(const char *s1, const char *s2);
-uint64_t            timeval_to_msec(struct timeval time);
-int                 timeval_cmp(struct timeval a, struct timeval b);
-struct timeval      timeval_add(struct timeval a, unsigned int b);
-void                print_status(const char *status, size_t number, t_config *conf);
-_Noreturn void      even_philosopher_run(t_philosopher *philosopher);
-_Noreturn void      odd_philosopher_run(t_philosopher *philosopher);
-_Noreturn void      *monitor_run(void *arg);
-int                 config(t_config *conf, int ac, char **av);
+size_t				ft_strlen(const char *s);
+short				ft_atos(const char *str);
+void				ft_putnbr(char *s, uint64_t n);
+void				ft_append(char *dest, const char *str);
+void				ft_putstr_fd(char const *s, int fd);
+int					ft_strcmp(const char *s1, const char *s2);
+uint64_t			timeval_to_msec(struct timeval time);
+int					timeval_cmp(struct timeval a, struct timeval b);
+struct timeval		timeval_add(struct timeval a, unsigned int b);
+void				print_status(const char *status, size_t number, t_config *conf);
+_Noreturn void		even_philosopher_run(t_philosopher *philosopher);
+_Noreturn void		odd_philosopher_run(t_philosopher *philosopher);
+_Noreturn void		*monitor_run(void *arg);
+int					config(t_config *conf, int ac, char **av);
 
 #endif
