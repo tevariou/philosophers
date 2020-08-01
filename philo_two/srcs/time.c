f89972d1fd364fa196bc65f2d2976d50fa052e06
Teva RIOU <teva@yusofleet.com>
2020-08-01 20:03:04 +0200

    norme

--- philo_two/srcs/time.c
+++ philo_two/srcs/time.c
@@ -2,11 +2,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-uint64_t		timeval_to_msec(struct timeval time) {
+uint64_t		timeval_to_msec(struct timeval time)
+{
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
-int             timeval_cmp(struct timeval a, struct timeval b) {
+int             timeval_cmp(struct timeval a, struct timeval b)
+{
 	if (a.tv_sec > b.tv_sec)
 		return (1);
 	if (a.tv_sec < b.tv_sec)
